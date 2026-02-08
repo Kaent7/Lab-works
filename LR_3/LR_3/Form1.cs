903986a9ffd3aab9bf127a23e803c44fe6224cb8
Artem Lyutikov <golkin794@gmail.com>
2026-02-08 18:45:55 +0300

    Добавил комментарии

--- LR_3/LR_3/Form1.cs
+++ LR_3/LR_3/Form1.cs
@@ -17,26 +17,34 @@ namespace LR_3
             InitializeComponent();
         }
 
+        // Метод по надатию кнопки
         private void button1_Click(object sender, EventArgs e)
         {
+            //Переменные
             double a, b, c;
             string cStr;
 
+            //Проверка правильности написания
             bool rez1 = double.TryParse(textBox1.Text, out a);
+            //Вывод сообщения об оштбке и выход из метода
             if (rez1 == false)
             {
                 MessageBox.Show($"Неверный формат числа: {textBox1.Text}!");
                 return;
-            } 
+            }
 
+            //Проверка правильности написания
             bool rez2 = double.TryParse(textBox2.Text, out b);
+            //Вывод сообщения об оштбке и выход из метода
             if (rez2 == false)
             {
                 MessageBox.Show($"Неверный формат числа: {textBox2.Text}!");
             }
 
+            //Решение
             c = (7 * a + 3 * b) * 4.5;
 
+            //Преоброзование в строку и вывод на форму
             cStr = c.ToString();
             label2.Text = cStr;
         }
