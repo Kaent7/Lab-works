9a2444666d50c2019b6345733a5cf1a932ef3699
fff <golkin794@gmail.com>
2026-02-17 11:44:31 +0300

    6 лабораторная (недоделана)

---
+++ LR_6/LR_6/FormAddUPDMD.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using LR_6.ModelEF;
+
+namespace LR_6
+{
+    public partial class FormAddUPDMD : Form
+    {
+        public FormAddUPDMD()
+        {
+            InitializeComponent();
+        }
+
+        private string Pic_Name;
+        private List<Table_Motorbike> vsMotorbike = FormShowMot.DB.Table_Motorbike.ToList();
+
+        private void FormAddUPDMD_Load(object sender, EventArgs e)
+        {
+            List<string> dictBrand = new List<string>();
+            foreach (Table_Motorbike TB in vsMotorbike)
+                dictBrand.Add(TB.Brand);
+            dictBrand = dictBrand.Distinct().ToList();
+            comboBoxBrand.DataSource = dictBrand;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonBack_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxPrice_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void textBoxHP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+
+        private void textBoxMileage_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+        }
+    }
+}
--- LR_6/LR_6/FormShowMot.Designer.cs
+++ LR_6/LR_6/FormShowMot.Designer.cs
@@ -28,22 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.iDDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.modelDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.brandDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.priceDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.horsepowerDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.mileageDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.tableMotorbikeBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.buttonAdd = new System.Windows.Forms.Button();
             this.buttonDel = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableMotorbikeBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AutoGenerateColumns = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.iDDataGridViewTextBoxColumn,
+            this.modelDataGridViewTextBoxColumn,
+            this.brandDataGridViewTextBoxColumn,
+            this.priceDataGridViewTextBoxColumn,
+            this.horsepowerDataGridViewTextBoxColumn,
+            this.mileageDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.tableMotorbikeBindingSource;
             this.dataGridView1.Location = new System.Drawing.Point(12, 12);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(496, 426);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // iDDataGridViewTextBoxColumn
+            // 
+            this.iDDataGridViewTextBoxColumn.DataPropertyName = "ID";
+            this.iDDataGridViewTextBoxColumn.HeaderText = "ID";
+            this.iDDataGridViewTextBoxColumn.Name = "iDDataGridViewTextBoxColumn";
+            // 
+            // modelDataGridViewTextBoxColumn
+            // 
+            this.modelDataGridViewTextBoxColumn.DataPropertyName = "Model";
+            this.modelDataGridViewTextBoxColumn.HeaderText = "Model";
+            this.modelDataGridViewTextBoxColumn.Name = "modelDataGridViewTextBoxColumn";
+            // 
+            // brandDataGridViewTextBoxColumn
+            // 
+            this.brandDataGridViewTextBoxColumn.DataPropertyName = "Brand";
+            this.brandDataGridViewTextBoxColumn.HeaderText = "Brand";
+            this.brandDataGridViewTextBoxColumn.Name = "brandDataGridViewTextBoxColumn";
+            // 
+            // priceDataGridViewTextBoxColumn
+            // 
+            this.priceDataGridViewTextBoxColumn.DataPropertyName = "Price";
+            this.priceDataGridViewTextBoxColumn.HeaderText = "Price";
+            this.priceDataGridViewTextBoxColumn.Name = "priceDataGridViewTextBoxColumn";
+            // 
+            // horsepowerDataGridViewTextBoxColumn
+            // 
+            this.horsepowerDataGridViewTextBoxColumn.DataPropertyName = "Horsepower";
+            this.horsepowerDataGridViewTextBoxColumn.HeaderText = "Horsepower";
+            this.horsepowerDataGridViewTextBoxColumn.Name = "horsepowerDataGridViewTextBoxColumn";
+            // 
+            // mileageDataGridViewTextBoxColumn
+            // 
+            this.mileageDataGridViewTextBoxColumn.DataPropertyName = "Mileage";
+            this.mileageDataGridViewTextBoxColumn.HeaderText = "Mileage";
+            this.mileageDataGridViewTextBoxColumn.Name = "mileageDataGridViewTextBoxColumn";
+            // 
+            // tableMotorbikeBindingSource
+            // 
+            this.tableMotorbikeBindingSource.DataSource = typeof(LR_6.ModelEF.Table_Motorbike);
             // 
             // pictureBox1
             // 
@@ -88,6 +147,7 @@
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.FormShowMot_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tableMotorbikeBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
@@ -99,6 +159,13 @@
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button buttonAdd;
         private System.Windows.Forms.Button buttonDel;
+        private System.Windows.Forms.DataGridViewTextBoxColumn iDDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn modelDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn brandDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn priceDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn horsepowerDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn mileageDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource tableMotorbikeBindingSource;
     }
 }
 
