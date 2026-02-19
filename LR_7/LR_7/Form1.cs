c8260cc9cd5ecc68029c6a0db8d657991755f935
Artem Lyutikov <golkin794@gmail.com>
2026-02-19 21:30:52 +0300

    7 лабораторная
    Ошибка в коде, который был прикреплен в ГК, исправил

---
+++ LR_7/LR_7/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Entity;
+
+namespace LR_7
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ModelEF.Model1 model = new ModelEF.Model1();
+
+        private void StartLoadData()
+        {
+            model.Users.Load();
+            role_NameComboBox.DataSource = model.Roles.ToList();
+            usersBindingSource.DataSource = model.Users.Local.ToBindingList();
+        }
+
+        private void SaveData()
+        {
+            try
+            {
+                Validate();
+                usersBindingSource.EndEdit();
+                usersBindingSource.ResetBindings(true);
+                model.SaveChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StartLoadData();
+        }
+
+        private void bindingNavigatorAddNewItem_Click(object sender, EventArgs e)
+        {
+            role_NameComboBox.SelectedIndex = 0;
+        }
+
+        private void bindingNavigatorDeleteItem_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+
+        private void usersBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            SaveData();
+        }
+    }
+}
